33cc271b47262a3604ae5ed6a222ac57107036d1
ali khan <alikhan40310@gmail.com>
2022-03-28 14:53:00 +0500

    Dynamic stepper content

--- src/components/SteppersVuetify.vue
+++ src/components/SteppersVuetify.vue
@@ -1,168 +1,44 @@
 <template>
-  <v-stepper v-model="e1" class=""  >
+  <v-stepper v-model="e1" class="mt-10">
     <v-stepper-header>
-      <v-stepper-step :complete="e1 > 1" step="1">
-        Question no 1
-      </v-stepper-step>
-
-      <v-divider></v-divider>
-
-      <v-stepper-step :complete="e1 > 2" step="2">
-        Question no 2
-      </v-stepper-step>
-
-      <v-divider></v-divider>
-
-      <v-stepper-step :complete="e1 > 3" step="3">
-        Question no 3
-      </v-stepper-step>
-      <v-divider></v-divider>
-
-      <v-stepper-step :complete="e1 > 4" step="4">
-        Question no 4
-      </v-stepper-step>
-      <v-divider></v-divider>
-
-      <v-stepper-step :complete="e1 > 5" step="5">
-        Question no 4
+      <v-stepper-step
+        :complete="question === index + 1"
+        v-for="(question, index) in questions"
+        :key="index"
+        :step="index + 1"
+      >
+        Question no {{ index + 1 }}
+        <v-divider></v-divider>
       </v-stepper-step>
     </v-stepper-header>
 
     <v-stepper-items>
-      <v-stepper-content step="1" v-if="index < count">
-        <div v-for="(answer, key) in questions[index]['answers']" :key="key">
-          <p class="text-2xl font-bold">
-            {{ questions[index]["question"] }}
-          </p>
+      <v-stepper-content 
+        v-for="(question, index) in questions"
+        :key="index"
+        :step="index + 1"
+      >
+        <h2 class="heading-style">
+          {{ question["question"] }}
+        </h2>
 
-          <v-radio
-            :label=" answer"
-            class="hidden"
-            :value="key"
-            @change="answered($event)"
+        <div v-for="(answer, ansindex) in question.answers" :key="ansindex">
+          <input
+            :id="ansindex"
+            type="radio"
+            :value="answer"
+            name="answer"
             v-model="selectedAnswer"
-            :disabled="selectedAnswer != ''"
-          ></v-radio>
+            class="radioStyle"
+          />
+          <label :for="ansindex">{{ answer }}</label>
         </div>
-        <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
 
-        <v-btn text @click="backFunc"> Back </v-btn>
-      </v-stepper-content>
+        <v-btn class="mt-4" color="primary" @click="e1 = index + 2">
+          Continue
+        </v-btn>
 
-      <v-stepper-content step="2">
-        <v-card>
-          <h1 class="text">
-            You’re really busy at work and a colleague is telling you their life
-            story and personal woes. You:
-          </h1>
-          <v-radio-group v-model="column" column>
-            <v-radio
-              label="  Don’t dare to interrupt them  "
-              value="second-1"
-            ></v-radio>
-            <v-radio
-              label="Think it’s more important to give them some of your time; work can wait  "
-              value="second-2"
-            ></v-radio>
-            <v-radio
-              label=" Listen, but with only with half an ear  "
-              value="second-3"
-            ></v-radio>
-            <v-radio
-              label="  Interrupt and explain that you are really busy at the moment  "
-              value="second-4"
-            ></v-radio>
-          </v-radio-group>
-        </v-card>
-        <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
-
-        <v-btn text @click="backFunc"> Back </v-btn>
-      </v-stepper-content>
-
-      <v-stepper-content step="3">
-        <v-card>
-          <h1 class="text">
-            You are taking part in a guided tour of a museum. You:
-          </h1>
-          <v-radio-group v-model="column" column>
-            <v-radio
-              label="Are a bit too far towards the back so don’t really hear what the guide is saying "
-              value="third-1"
-            ></v-radio>
-            <v-radio
-              label="Follow the group without question "
-              value="third-2"
-            ></v-radio>
-            <v-radio
-              label=" Make sure that everyone is able to hear properly  "
-              value="third-3"
-            ></v-radio>
-            <v-radio
-              label="Are right up the front, adding your own comments in a loud voice "
-              value="third-4"
-            ></v-radio>
-          </v-radio-group>
-        </v-card>
-        <v-btn color="primary" @click="e1 = 4"> Continue </v-btn>
-
-        <v-btn text @click="backFunc"> Back </v-btn>
-      </v-stepper-content>
-      <v-stepper-content step="4">
-        <v-card>
-          <h1 class="text">
-            During dinner parties at your home, you have a hard time with people
-            who:
-          </h1>
-          <v-radio-group v-model="column" column>
-            <v-radio
-              label=" Ask you to tell a story in front of everyone else "
-              value="fourth-1"
-            ></v-radio>
-            <v-radio
-              label=" Talk privately between themselves "
-              value="fourth-2"
-            ></v-radio>
-            <v-radio
-              label="Hang around you all evening  "
-              value="fourth-3"
-            ></v-radio>
-            <v-radio
-              label="Always drag the conversation back to themselves  "
-              value="fourth-4"
-            ></v-radio>
-          </v-radio-group>
-        </v-card>
-        <v-btn color="primary" @click="e1 = 5"> Continue </v-btn>
-
-        <v-btn text @click="backFunc"> Back </v-btn>
-      </v-stepper-content>
-      <v-stepper-content step="5">
-        <v-card>
-          <h1 class="text">
-            You crack a joke at work, but nobody seems to have noticed. You:
-          </h1>
-          <v-radio-group v-model="column" column>
-            <v-radio
-              label=" Think it’s for the best — it was a lame joke anyway  "
-              value="fivth-1"
-            ></v-radio>
-            <v-radio
-              label="Wait to share it with your friends after work  "
-              value="fivth-2"
-            ></v-radio>
-            <v-radio
-              label="Try again a bit later with one of your colleagues  "
-              value="fivth-3"
-            ></v-radio>
-            <v-radio
-              label=" Keep telling it until they pay attention "
-              value="fivth-4"
-            ></v-radio>
-          </v-radio-group>
-        </v-card>
-        <v-btn color="primary" @click="e1 = 6"> Continue </v-btn>
-
-        <v-btn text @click="backFunc"> Back </v-btn>
+        <v-btn class="mt-4" text @click="backFunc"> Back </v-btn>
       </v-stepper-content>
     </v-stepper-items>
   </v-stepper>
@@ -177,37 +53,60 @@ export default {
       index: 0,
       selectedAnswer: "",
       correctAnswer: 0,
-      wrongAnswer: 0,
-      count: 3,
       questions: [
         {
-          question: "Q1. Who create python  language?",
-          answers: {
-            a: "Guido van Rossum",
-            b: "Kristen Nygaard",
-            c: "Bjarne Stroustrup",
-            d: "Ole-Johan Dahl",
-          },
+          question:
+            "Q1. You’re really busy at work and a colleague is telling you their life story and personal woes. You:Q1. Who create python language?",
+          answers: [
+            "Don’t dare to interrupt them",
+            "Think it’s more important to give them some of your time; work can wait",
+            "Listen, but with only with half an ear",
+            "Interrupt and explain that you are really busy at the moment",
+          ],
           correctAnswer: "a",
         },
         {
-          question: "Q2. What does PEP stand for in python?",
-          answers: {
-            a: "name of parentehes parameters",
-            b: "python english processing",
-            c: "Python Enhancement Proposal",
-            d: "PyPi",
-          },
+          question:
+            "Q2. You’ve been sitting in the doctor’s waiting room for more than 25 minutes. You:",
+          answers: [
+            "Look at your watch every two minutes",
+            "Bubble with inner anger, but keep quiet",
+            "Explain to other equally impatient people in the room that the doctor is always running late",
+            "Complain in a loud voice, while tapping your foot impatiently",
+          ],
           correctAnswer: "c",
         },
         {
-          question: "Q3. What are python modules?",
-          answers: {
-            a: "Are .py files with executable code",
-            b: "Python packages",
-            c: "Python list objects",
-            d: "Python packages",
-          },
+          question:
+            "Q3. You’re having an animated discussion with a colleague regarding a project that you’re in charge of. You:",
+          answers: [
+            "Don’t dare contradict them",
+            "Think that they are obviously right",
+            "Defend your own point of view, tooth and nail",
+            "Continuously interrupt your colleague",
+          ],
+          correctAnswer: "a",
+        },
+        {
+          question:
+            "Q4. You are taking part in a guided tour of a museum. You:",
+          answers: [
+            "Are a bit too far towards the back so don’t really hear what the guide is saying",
+            "Follow the group without question",
+            "Make sure that everyone is able to hear properly",
+            "Are right up the front, adding your own comments in a loud voice",
+          ],
+          correctAnswer: "a",
+        },
+        {
+          question:
+            "Q5. During dinner parties at your home, you have a hard time with people who:",
+          answers: [
+            "Ask you to tell a story in front of everyone else",
+            "Talk privately between themselves",
+            "Hang around you all evening",
+            "Always drag the conversation back to themselves",
+          ],
           correctAnswer: "a",
         },
       ],
@@ -219,13 +118,6 @@ export default {
         this.e1--;
       }
     },
-
-    answered(e) {
-      this.selectedAnswer = e.target.value;
-      if (this.selectedAnswer == this.questions[this.index]["correctAnswer"])
-        this.correctAnswer++;
-      else this.wrongAnswer++;
-    },
   },
 };
 </script>
@@ -236,16 +128,33 @@ export default {
   font-weight: 500;
   margin-bottom: 20px;
 }
-.v-stepper{
+.v-stepper {
 }
-.v-sheet.v-stepper:not(.v-sheet--outlined){
-  box-shadow:0px 3px 1px -2px transparent;
+.v-sheet.v-stepper:not(.v-sheet--outlined) {
+  box-shadow: 0px 3px 1px -2px transparent;
   /* outline: none;  */
   -webkit-box-shadow: 0px 3px 1px -2px transparent;
   /* background: red; */
 }
-.v-stepper__content{
+.v-stepper__content {
   /* box shadow transparent */
   box-shadow: 0px 3px 1px -2px transparent;
+}
+.radioStyle {
+  margin-right: 10px;
+  font-size: 21rem;
+  width: 1.2rem;
+  height: 1.2rem;
+  border-radius: 50%;
+  accent-color: #2196f3;
+  margin-top: 0.5rem;
+}
+label {
+  font-size: 1.2rem;
+}
+.heading-style {
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-bottom: 1rem;
 }
 </style>
